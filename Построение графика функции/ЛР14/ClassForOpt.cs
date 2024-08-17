dd00195954403e3b9a36cd1084a445fce0db1a40
0-kartOshka-0 <darazajceva3675@mail.ru>
2024-08-17 15:53:44 +0300

    Graph of a function

---
+++ Построение графика функции/ЛР14/ClassForOpt.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms.DataVisualization.Charting;
+
+namespace ЛР14
+{
+    internal class ClassForOpt
+    {
+        static ClassForOpt()
+        {
+            Legend = true;
+            Title = true;
+            Width = 2;
+            ChartType = SeriesChartType.Spline;
+            LineColor = Color.Aqua;
+            BackgroundColor = Color.White;
+        }
+        public static  bool Legend;
+        public static  bool Title;
+        public static  int Width;
+        public static  SeriesChartType ChartType;
+        public static  Color LineColor;
+        public static  Color BackgroundColor;
+    }
+}
